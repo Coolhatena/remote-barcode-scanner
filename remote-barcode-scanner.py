dcd22af28513b3cce6b36d9214f307235df7a354
Coolhatena <ingsc.geffroy@gmail.com>
2025-06-21 13:11:01 -0700

    First remote implementation

---
+++ remote-barcode-scanner.py
@@ -0,0 +1,44 @@
+import pyautogui
+import json
+import socket
+import os
+
+def loadConfig():
+	script_dir = os.path.dirname(os.path.abspath(__file__))
+	config_path = os.path.join(script_dir, 'config.json')
+	with open(config_path, 'r') as configFile:
+		configData = json.load(configFile)
+
+	print("Configuración: ")
+	print(configData)
+	return configData
+
+while True: 
+	try:
+		configData = loadConfig()
+		host = configData["ip"]
+		port = configData["port"]
+		EOF = "EOF"
+		server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		server_socket.bind((host, port))
+		server_socket.listen(1)
+		print(f'Servidor escuchando en {host}:{port}')
+
+		while True:
+			data = None
+			client_socket, client_address = server_socket.accept()
+			print(f'Conexión aceptada de {client_address}')
+			while True:
+				message = client_socket.recv(1024).decode('utf-8')
+				if message:
+					pyautogui.write(message)
+					pyautogui.press("enter")
+				else:
+					print('No se recibió ningún dato.')
+
+				client_socket.close()
+				print(f'Conexión cerrada con {client_address}')
+				break
+	except Exception as e:
+		print(f"Ocurrió un error: {e}")
